efb7fad0d9ba89fe5d8883e90cfd1bd0f3b7fc48
JRKeck <joshrkeck@gmail.com>
2015-08-06 10:55:01 -0500

    initial commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var cohortData = require("../public/assets/data/gamma_cohort.json");
+var modAdj = require("../public/assets/data/modifying_adjectives.json");
+var adj = require("../public/assets/data/adjectives.json");
+var javaScript = require("../public/assets/data/javascript.json");
 
-router.get('/data', function(req, res){
-    res.json(cohortData);
+
+router.get('/data1', function(req, res){
+    res.json(modAdj);
+});
+
+router.get('/data2', function(req, res){
+    res.json(adj);
+});
+
+router.get('/data3', function(req, res){
+    res.json(javaScript);
 });
 
 router.get("/*", function(req, res){
